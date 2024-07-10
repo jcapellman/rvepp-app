846501f7c5c4c09da1c4b051dd97160491ae11d1
Jarred Capellman <jcapellman@hotmail.com>
2024-07-10 18:13:26 -0500

    Change: Config properties are now public

--- crates/rvepp_configuration/src/lib.rs
+++ crates/rvepp_configuration/src/lib.rs
@@ -2,7 +2,7 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
-    management_url: String,
-    rtfm: bool
+    pub management_url: String,
+    pub rtfm: bool
 }
 
