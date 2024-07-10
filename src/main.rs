2f71521c3715c2b0853260822e66f99125b7e02a
Jarred Capellman <jcapellman@hotmail.com>
2024-07-10 18:15:10 -0500

    Added: Protection Layer Manager reference in the main (WIP)

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 #[cfg(target_os = "linux")]
-use rvepp_protection_layers::{ProtectionLayer, rtfm};
-
-#[cfg(target_os = "linux")]
 fn main() {
-    let mut real_time_file_monitoring = rtfm::Rtfm { };
+    let config = rvepp_configuration::Config {
+        management_url: "https://rvepp.io".to_string(),
+        rtfm: true
+    };
 
-    real_time_file_monitoring.initialize();
+    let protection_manager = rvepp_protection_layers::protection_layer_manager::ProtectionLayerManager { };
+
+    let initialize_result = protection_manager.initialize(config);
+
+    match initialize_result {
+        Some(true) => println!("Initialized the Protection Manager"),
+        Some(false) => println!("Failed to Initialize the Protection Manager"),
+        None => println!("Error occurred")
+    }
 }
 
 #[cfg(not(target_os = "linux"))]
