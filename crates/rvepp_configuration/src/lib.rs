14e55f8871c57d0af5d087bb68d971eb210ffaf8
Jarred Capellman <jcapellman@hotmail.com>
2024-07-10 22:03:50 -0500

    Change: Refactoring the loading to make it easier to unit test

--- crates/rvepp_configuration/src/lib.rs
+++ crates/rvepp_configuration/src/lib.rs
@@ -7,21 +7,7 @@ pub struct Config {
     pub rtfm: bool
 }
 
-pub fn load_config() -> Option<Config> {
-    match fs::create_dir_all(rvepp_common::CONFIGURATION_BASE_PATH) {
-        Ok(_) => {
-            println!("Configuration path did not exist and was created");
-        }
-        Err(err) => {
-            println!("Error creating the configuration due to: {err}");
-
-            return None;
-        }
-    };
-
-    let json_string = fs::read_to_string(rvepp_common::CONFIGURATION_BASE_PATH.to_string() + rvepp_common::CONFIGURATION_FILE_NAME)
-        .expect("Should have been able to read the file");
-
+fn json_to_config(json_string: String) -> Option<Config> {
     return match serde_json::from_str::<Config>(&json_string) {
         Ok(config) => {
             Some(config)
@@ -32,4 +18,30 @@ pub fn load_config() -> Option<Config> {
             None
         }
     };
+}
+
+fn validate_path() -> Option<bool> {
+    return match fs::create_dir_all(rvepp_common::CONFIGURATION_BASE_PATH) {
+        Ok(_) => {
+            println!("Configuration path did not exist and was created");
+
+            Some(true)
+        }
+        Err(err) => {
+            println!("Error creating the configuration due to: {err}");
+
+            None
+        }
+    };
+}
+
+pub fn load_config() -> Option<Config> {
+    if validate_path() == None {
+        return None;
+    }
+
+    let json_string = fs::read_to_string(rvepp_common::CONFIGURATION_BASE_PATH.to_string() + rvepp_common::CONFIGURATION_FILE_NAME)
+        .expect("Should have been able to read the file");
+
+    return json_to_config(json_string);
 }
