27ae37b3455d8d128a737e5f26fa800fde78a4e1
Jarred Capellman <jcapellman@hotmail.com>
2024-07-10 21:23:25 -0500

    Change: main module is now cleaner with just a 1 liner to the CLI Crate

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,6 @@
 #[cfg(target_os = "linux")]
 fn main() {
-    let config = rvepp_configuration::Config {
-        management_url: "https://rvepp.io".to_string(),
-        rtfm: true
-    };
-
-    let protection_manager = rvepp_protection_layers::protection_layer_manager::ProtectionLayerManager { };
-
-    let initialize_result = protection_manager.initialize(config);
-
-    match initialize_result {
-        Some(true) => println!("Initialized the Protection Manager"),
-        Some(false) => println!("Failed to Initialize the Protection Manager"),
-        None => println!("Error occurred")
-    }
+    rvepp_cli::run();
 }
 
 #[cfg(not(target_os = "linux"))]
