6665675cc772b23053b55e23eadc677498672751
Jarred Capellman <jcapellman@hotmail.com>
2024-07-10 21:53:55 -0500

    Added: load_config method to load the config from a json file

--- crates/rvepp_configuration/src/lib.rs
+++ crates/rvepp_configuration/src/lib.rs
@@ -1,4 +1,5 @@
 use serde::{Serialize, Deserialize};
+use std::fs;
 
 #[derive(Serialize, Deserialize)]
 pub struct Config {
@@ -6,3 +7,29 @@ pub struct Config {
     pub rtfm: bool
 }
 
+pub fn load_config() -> Option<Config> {
+    match fs::create_dir_all(rvepp_common::CONFIGURATION_BASE_PATH) {
+        Ok(_) => {
+            println!("Configuration path did not exist and was created");
+        }
+        Err(err) => {
+            println!("Error creating the configuration due to: {err}");
+
+            return None;
+        }
+    };
+
+    let json_string = fs::read_to_string(rvepp_common::CONFIGURATION_BASE_PATH.to_string() + rvepp_common::CONFIGURATION_FILE_NAME)
+        .expect("Should have been able to read the file");
+
+    return match serde_json::from_str::<Config>(&json_string) {
+        Ok(config) => {
+            Some(config)
+        }
+        Err(err) => {
+            println!("Error creating the configuration due to: {err}");
+
+            None
+        }
+    };
+}
