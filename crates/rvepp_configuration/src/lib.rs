1f92df5dda7cf34164dd61d0c898290f4414cb9e
Jarred Capellman <jcapellman@hotmail.com>
2024-07-10 22:26:12 -0500

    Added: Happy and Unhappy paths on Config Reading and Writing

--- crates/rvepp_configuration/src/lib.rs
+++ crates/rvepp_configuration/src/lib.rs
@@ -35,13 +35,45 @@ fn validate_path() -> Option<bool> {
     };
 }
 
+fn create_default_config() -> Config {
+    return Config {
+        management_url: "https://rvepp.io".to_string(),
+        rtfm: true
+    };
+}
+
+fn create_default_file(full_path: String) -> Option<Config> {
+    let config = create_default_config();
+
+    let json_string = serde_json::to_string(&config).expect("Failure in serializing the configuration");
+
+    return match fs::write(full_path.clone(), json_string) {
+        Ok(_) => Some(config),
+        Err(err) => {
+            println!("Error writing default config to {full_path} due to: {err}");
+
+            None
+        }
+    };
+}
+
 pub fn load_config() -> Option<Config> {
     if validate_path() == None {
         return None;
     }
 
-    let json_string = fs::read_to_string(rvepp_common::CONFIGURATION_BASE_PATH.to_string() + rvepp_common::CONFIGURATION_FILE_NAME)
-        .expect("Should have been able to read the file");
+    let full_path = rvepp_common::CONFIGURATION_BASE_PATH.to_string() + rvepp_common::CONFIGURATION_FILE_NAME;
 
-    return json_to_config(json_string);
+    if !fs::metadata(full_path.clone()).is_ok() {
+        return create_default_file(full_path);
+    }
+
+    return match fs::read_to_string(&full_path) {
+        Ok(json_string) => json_to_config(json_string),
+        Err(err) => {
+            println!("Failed to read configuration file: {err}");
+
+            None
+        }
+    }
 }
