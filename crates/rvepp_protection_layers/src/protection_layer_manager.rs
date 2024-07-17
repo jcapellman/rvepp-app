e5723868ab0f44884c18024d5d749243edc24290
Jarred Capellman <jcapellman@hotmail.com>
2024-07-16 20:58:29 -0500

    Added: Logging on RTFM

--- crates/rvepp_protection_layers/src/protection_layer_manager.rs
+++ crates/rvepp_protection_layers/src/protection_layer_manager.rs
@@ -8,9 +8,13 @@ pub struct ProtectionLayerManager {
 impl ProtectionLayerManager {
     pub fn initialize(&self, config: rvepp_configuration::Config) -> Option<bool> {
         if config.rtfm {
+            println!("Initializing RTFM...");
+
             let mut real_time_monitoring = Rtfm { };
 
             real_time_monitoring.initialize();
+
+            println!("RTFM Initialized...");
         }
 
         return Some(true);
