6cae9d9ebb9f98d668c2ad111e4ab4cb6eb66334
Jarred Capellman <jcapellman@hotmail.com>
2024-07-20 20:53:47 -0500

    Added: Default ELF Model File Name to the Constants

--- crates/rvepp_common/src/constants.rs
+++ crates/rvepp_common/src/constants.rs
@@ -3,3 +3,5 @@ pub const CONFIGURATION_FILE_NAME: &str = "config.json";
 pub const CONFIGURATION_BASE_PATH: &str = "/etc/rvepp/";
 
 pub const CONFIGURATION_URL: &str = "https://rvepp.io/";
+
+pub const CONFIGURATION_MODEL_ELF_FILE_NAME: &str = "elf.model";
